4cdcb4edafda035f0d5ba4968e0c3a1060c832b4
whoisaymen <aymen@qq.com>
2023-08-24 14:29:18 +0200

    cache no-store

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 // 'use client';
 import ProductList from '@/components/ProductList';
-import { getProductsInCollection } from '@/lib/shopify';
+import { getProductsInCollection, getAllProducts } from '@/lib/shopify';
 import Hero from '@/components/Hero';
 
 export default async function Home() {
 	const products = await getProductsInCollection();
+	const allProducts = await getAllProducts();
+	console.log('get products in collection:', products);
 
 	return (
 		<div className='text-xl'>
