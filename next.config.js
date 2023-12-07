d27a7c8e8426ed1807e9180426efca5037ac1eeb
Aymen <aymen@qq.com>
2023-12-08 00:14:31 +0700

    last edits

--- next.config.js
+++ next.config.js
@@ -1,12 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	env: {
-		SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
-		SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN,
-	},
-	images: {
-		domains: ['cdn.shopify.com'],
-	},
+  env: {
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN,
+  },
+  images: {
+    domains: ["cdn.shopify.com"],
+  },
+  async headers() {
+    return [
+      {
+        // Routes this applies to
+        source: "/api/(.*)",
+        // Headers
+        headers: [
+          // Allow for specific domains to have access or * for all
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+            // DOES NOT WORK
+            // value: process.env.ALLOWED_ORIGIN,
+          },
+          // Allows for specific methods accepted
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          // Allows for specific headers accepted (These are a few standard ones)
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
