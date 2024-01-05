84822a3706019036cf743babd47ed27b2d7f2815
Aymen <aymen@qq.com>
2024-01-05 12:08:26 +0700

    deleted unnecesarry motion divs to productlist

--- components/ProductList.js
+++ components/ProductList.js
@@ -9,24 +9,22 @@ const ProductList = ({ products }) => {
   const sortedProducts = sortProducts(products, sortOption);
 
   return (
-    <motion.div
+    <div
       className="mb-[24px] md:mb-[29px] md:mx-0 -mt-[2px]"
       style={{ backgroundImage: "url(/images/bgHomeGradient.svg)" }}
     >
       <div className="mx-auto">
-        <AnimatePresence>
-          <motion.div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 -mt-3">
-            {sortedProducts.map((product, index) => (
-              <ProductCard
-                key={product.node.id}
-                product={product}
-                index={index}
-              />
-            ))}
-          </motion.div>
-        </AnimatePresence>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 -mt-3">
+          {sortedProducts.map((product, index) => (
+            <ProductCard
+              key={product.node.id}
+              product={product}
+              index={index}
+            />
+          ))}
+        </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
