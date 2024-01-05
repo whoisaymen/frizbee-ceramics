12b9eb615aefde77879d9820a0865b3c4d6438ce
Aymen <aymen@qq.com>
2024-01-05 12:53:11 +0700

    first go at reworking sorting logic

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,12 +1,10 @@
-"use client";
-import { useContext, useEffect, useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
 import ProductCard from "./ProductCard";
-import { CartContext } from "@/context/shopContext";
+import { getProductsInCollection } from "@/lib/shopify";
 
-const ProductList = ({ products }) => {
-  const { sortOption, sortProducts } = useContext(CartContext);
-  const sortedProducts = sortProducts(products, sortOption);
+const ProductList = async () => {
+  const products = await getProductsInCollection();
+
+  // const sortedProducts = sortProducts(products, "new");
 
   return (
     <div
@@ -15,7 +13,7 @@ const ProductList = ({ products }) => {
     >
       <div className="mx-auto">
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 -mt-3">
-          {sortedProducts.map((product, index) => (
+          {products.map((product, index) => (
             <ProductCard
               key={product.node.id}
               product={product}
@@ -27,5 +25,63 @@ const ProductList = ({ products }) => {
     </div>
   );
 };
+
+function sortProducts(products, option) {
+  if (option === "color") {
+    return [...products].sort((a, b) => {
+      const colorA =
+        a.node.tags.find((tag) => tag.startsWith("color:"))?.split(":")[1] ||
+        "";
+      const colorB =
+        b.node.tags.find((tag) => tag.startsWith("color:"))?.split(":")[1] ||
+        "";
+      return colorA.localeCompare(colorB);
+    });
+  }
+  if (option === "shape") {
+    return [...products].sort((a, b) => {
+      const shapeA =
+        a.node.tags.find((tag) => tag.startsWith("shape:"))?.split(":")[1] ||
+        "";
+      const shapeB =
+        b.node.tags.find((tag) => tag.startsWith("shape:"))?.split(":")[1] ||
+        "";
+      return shapeA.localeCompare(shapeB);
+    });
+  }
+  if (option === "pattern") {
+    return [...products].sort((a, b) => {
+      const patternA =
+        a.node.tags.find((tag) => tag.startsWith("pattern:"))?.split(":")[1] ||
+        "";
+      const patternB =
+        b.node.tags.find((tag) => tag.startsWith("pattern:"))?.split(":")[1] ||
+        "";
+      return patternA.localeCompare(patternB);
+    });
+  }
+
+  if (option === "set") {
+    const filteredProducts = [...products].filter((product) => {
+      return product.node.tags.includes("set");
+    });
+    return filteredProducts;
+  }
+  if (option === "new") {
+    // Filter products with the 'new' tag
+    return products.filter((product) => product.node.tags.includes("new"));
+  }
+
+  if (option === "date") {
+    // Sort products by date added, from newest to oldest
+    return [...products].sort((a, b) => {
+      const dateA = new Date(a.node.createdAt);
+      const dateB = new Date(b.node.createdAt);
+      return dateB - dateA; // Sorting in descending order
+    });
+  }
+
+  return products; // return products as-is if there's no sorting
+}
 
 export default ProductList;
