7fcecb60ae2063dcfabb22d41b56ea9204b9c0c3
Aymen <aymen@qq.com>
2024-01-31 19:56:42 +0700

    rearranged some images

--- app/projects/[project]/page.jsx
+++ app/projects/[project]/page.jsx
@@ -5,6 +5,10 @@ import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "@/components/ProductCard";
 import { getProductsForProject } from "@/lib/shopify";
+import img1 from "/public/images/projects/a-box-is-a-box/img1.jpg";
+import img2 from "/public/images/projects/a-box-is-a-box/img2.png";
+import screenshot1 from "/public/images/projects/a-box-is-a-box/screenshot1.png";
+import screenshot2 from "/public/images/projects/a-box-is-a-box/screenshot2.png";
 
 export async function generateMetadata({ params }) {
   const projectDetails = projectsData.find((p) => p.id === params.project);
@@ -28,7 +32,7 @@ export default async function ProjectPage({ params }) {
       <ScrollToTop />
 
       <div className="grid grid-cols-1 md:grid-cols-2 -m-[1px] h-screen">
-        {projectDetails.images.slice(0, 2).map((img, index) => (
+        {/* {projectDetails.images.slice(0, 2).map((img, index) => (
           <div
             key={img.id}
             className="border-l border-black border-b relative h-full w-full"
@@ -40,11 +44,28 @@ export default async function ProjectPage({ params }) {
               className={`relative h-full w-full object-cover`}
             />
           </div>
-        ))}
+        ))} */}
+        <div className="border-l border-black border-b relative h-full w-full">
+          <Image
+            src={img1}
+            alt="A Box is a Box"
+            fill
+            placeholder="blur"
+            className={`relative h-full w-full object-cover`}
+          />
+        </div>
+        <div className="hidden md:block border-l border-black border-b relative h-full w-full">
+          <Image
+            src={img2}
+            alt="A Box is a Box"
+            fill
+            placeholder="blur"
+            className={`relative h-full w-full object-cover`}
+          />
+        </div>
       </div>
 
-      {/* Second Section - Text on the Left, Image on the Right */}
-      <div className="h-screen">
+      <div className="h-screen hidden md:block">
         <div className="grid grid-cols-1 md:grid-cols-2 h-full mt-0 -m-[1px]">
           <div className="flex flex-col justify-start p-28 pl-10 pr-32">
             <h2 className="text-2xl lg:text-5xl font-extralight tracking-tighter mb-10 leading-snug">
@@ -54,7 +75,7 @@ export default async function ProjectPage({ params }) {
           <div className="border-l border-t border-black relative">
             <Image
               src={projectDetails.images[2].src}
-              alt={projectDetails.images[2].alt}
+              alt="A Box is a Box"
               // height={1000}
               // width={1000}
               fill
@@ -72,17 +93,23 @@ export default async function ProjectPage({ params }) {
           {projectDetails.description[0]}
         </p>
       </div>
+      <div className="grid grid-cols-2 lg:grid-cols-4 -mt-3 border-t border-black">
+        {products.slice(0, 4).map((product, index) => (
+          <ProductCard key={product.node.id} product={product} index={index} />
+        ))}
+      </div>
 
-      <div className="flex flex-col justify-end h-full">
-        <div className="grid grid-cols-2 lg:grid-cols-4 -mt-3 border-t border-black">
-          {products.slice(0, 4).map((product, index) => (
-            <ProductCard
-              key={product.node.id}
-              product={product}
-              index={index}
-            />
-          ))}
+      <div className="h-screen">
+        <div className="relative h-full w-full">
+          <Image
+            src={screenshot1}
+            alt="A Box is a Box"
+            fill
+            placeholder="blur"
+            className={`h-full w-full object-cover`}
+          />
         </div>
+
         {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
           {projectDetails.productImages.slice(0, 4).map((image, index) => (
             <div key={image.id} className="relative w-full h-[50vh]">
@@ -102,26 +129,35 @@ export default async function ProjectPage({ params }) {
         </div> */}
       </div>
 
-      <div className="h-screen border-b border-black flex items-center justify-center p-4 pr-30 max-w-6xl">
-        <p className="text-xl lg:text-3xl font-extralight tracking-tighter mb-4 uppercase">
+      <div className="h-screen flex items-center justify-center p-4 pr-30 w-full">
+        <p className="text-xl lg:text-3xl font-extralight tracking-tighter mb-4 uppercase  max-w-6xl">
           {projectDetails.description[1]}
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 -m-[1px] h-screen">
-        {projectDetails.images.slice(3, 5).map((img, index) => (
+      <div className="h-screen">
+        {/* {projectDetails.images.slice(3, 5).map((img, index) => (
           <div
             key={img.id}
             className="border-l border-black border-b relative h-full w-full"
           >
             <Image
               src={img.src}
-              alt={img.alt}
+              alt="A Box is a Box"
               fill
               className={`relative h-full w-full object-cover`}
             />
           </div>
-        ))}
+        ))} */}
+        <div className="relative h-full w-full border-t border-black">
+          <Image
+            src={screenshot2}
+            alt="A Box is a Box"
+            fill
+            placeholder="blur"
+            className={`h-full w-full object-cover`}
+          />
+        </div>
       </div>
     </div>
   );
