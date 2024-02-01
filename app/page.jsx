dd727559e1b007501d765ef39b06772eead54d06
Aymen <aymen@qq.com>
2024-02-01 19:54:15 +0700

    fixes typo on home component

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,13 @@
-// import ProductList from "@/components/ProductList";
-import dynamic from "next/dynamic";
+import ProductList from "@/components/ProductList";
 import { CardsSkeleton } from "@/components/Skeleton";
 import { Suspense } from "react";
 
-const ProductList = dynamic(() => import("@/components/ProductList"), {
-  loading: () => <CardsSkeleton />,
-  ssr: false,
-});
-
-export default async function Products({ searchParams }) {
+export default function Home({ searchParams }) {
   const sortOption = searchParams.sort || "default";
 
-  return <ProductList sortOption={sortOption} />;
+  return (
+    <Suspense fallback={<CardsSkeleton />}>
+      <ProductList sortOption={sortOption} />
+    </Suspense>
+  );
 }
