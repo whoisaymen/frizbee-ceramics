65ba9b7a54058e3e8d3ee39f2e814015cd565e30
Aymen <aymen@qq.com>
2024-02-01 19:29:34 +0700

    uses dynamic imports with loading state instead of suspense

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,15 @@
-import ProductList from "@/components/ProductList";
+// import ProductList from "@/components/ProductList";
+import dynamic from "next/dynamic";
 import { CardsSkeleton } from "@/components/Skeleton";
 import { Suspense } from "react";
+
+const ProductList = dynamic(() => import("@/components/ProductList"), {
+  loading: () => <CardsSkeleton />,
+  ssr: false,
+});
 
 export default async function Products({ searchParams }) {
   const sortOption = searchParams.sort || "default";
 
-  return (
-    <Suspense fallback={<CardsSkeleton />}>
-      <ProductList sortOption={sortOption} />
-    </Suspense>
-  );
+  return <ProductList sortOption={sortOption} />;
 }
