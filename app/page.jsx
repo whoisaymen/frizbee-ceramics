1903e8cd08d30f7ab862ef4bb16edbf1cb159c9c
Aymen <aymen@qq.com>
2024-03-06 16:07:45 +0800

    useSearchParams vercel deployment fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import ProductList from "@/components/ProductList";
 import { CardsSkeleton } from "@/components/Skeleton";
 import { Suspense } from "react";
+export const dynamic = "force-dynamic";
 
 export default function Home({ searchParams }) {
   const sortOption = searchParams.sort || "default";
