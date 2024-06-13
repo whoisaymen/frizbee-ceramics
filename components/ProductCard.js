0f7d17fb1dd108d0d8404678b4f07f48b6a92f8c
Aymen <aymen@qq.com>
2024-06-13 15:23:49 +0200

    fixes homepage bug on iOS

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,40 +1,40 @@
-"use client";
-import { cubicBezier } from "framer-motion";
-import { motion } from "framer-motion";
-import Image from "next/image";
-import Link from "next/link";
-import dynamic from "next/dynamic";
-import { useState, useContext, useEffect } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
-import { CartContext } from "../context/shopContext";
+'use client'
+import { cubicBezier } from 'framer-motion'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import Link from 'next/link'
+import dynamic from 'next/dynamic'
+import { useState, useContext, useEffect } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper/modules'
+import { CartContext } from '../context/shopContext'
 
-import { formatter, getColorFromTag, colorMappings } from "../utils/helpers";
+import { formatter, getColorFromTag, colorMappings } from '../utils/helpers'
 
-import MobileQuickBuy from "./ui/MobileQuickBuy";
-import DesktopQuickBuy from "./ui/DesktopQuickBuy";
+import MobileQuickBuy from './ui/MobileQuickBuy'
+import DesktopQuickBuy from './ui/DesktopQuickBuy'
 
 // const MobileQuickBuy = dynamic(() => import("./ui/MobileQuickBuy"));
 // const DesktopQuickBuy = dynamic(() => import("./ui/DesktopQuickBuy"));
 
 const ProductCard = ({ product, index }) => {
-  const { addToCart } = useContext(CartContext);
-  const { handle, title, id } = product.node;
-  const price = product.node.priceRange.minVariantPrice.amount;
+  const { addToCart } = useContext(CartContext)
+  const { handle, title, id } = product.node
+  const price = product.node.priceRange.minVariantPrice.amount
   const compareAtPrice =
-    product.node.compareAtPriceRange?.minVariantPrice.amount;
-  const isAvailableForSale = product.node.availableForSale;
-  const colorValue = getColorFromTag(product);
-  const imageUrl1 = product.node.images.edges[0].node.url;
-  const imageUrl2 = product.node.images.edges[1]?.node.url;
-  const [displayImageUrl, setDisplayImageUrl] = useState(imageUrl1);
+    product.node.compareAtPriceRange?.minVariantPrice.amount
+  const isAvailableForSale = product.node.availableForSale
+  const colorValue = getColorFromTag(product)
+  const imageUrl1 = product.node.images.edges[0].node.url
+  const imageUrl2 = product.node.images.edges[1]?.node.url
+  const [displayImageUrl, setDisplayImageUrl] = useState(imageUrl1)
 
   const handleAddToCart = () => {
-    const defaultVariant = product.node.variants.edges[0]?.node;
-    const allOptions = {};
+    const defaultVariant = product.node.variants.edges[0]?.node
+    const allOptions = {}
     defaultVariant.selectedOptions.map((item) => {
-      allOptions[item.name] = item.value;
-    });
+      allOptions[item.name] = item.value
+    })
 
     const itemToAdd = {
       id: defaultVariant.id,
@@ -45,26 +45,27 @@ const ProductCard = ({ product, index }) => {
       variantPrice: defaultVariant.priceV2.amount,
       variantQuantity: 1,
       options: allOptions,
-    };
+    }
 
-    if (!itemToAdd) return;
-    addToCart(itemToAdd);
-  };
+    if (!itemToAdd) return
+    addToCart(itemToAdd)
+  }
 
   const SwiperSlides = () => {
     return product.node.images.edges.map((image, i) => (
       <SwiperSlide key={`mobile-slide-${i}`}>
         <Image
           src={image.node.url}
-          alt="Product image"
-          width={500}
-          height={500}
-          priority={i === 0 ? "true" : "false"}
-          className="w-full h-full object-cover object-center"
+          alt='Product image'
+          width={300}
+          height={300}
+          // priority={i === 0 ? 'true' : 'false'}
+          loading='lazy'
+          className='w-full h-full object-cover object-center'
         />
       </SwiperSlide>
-    ));
-  };
+    ))
+  }
 
   return (
     <motion.div
@@ -73,52 +74,52 @@ const ProductCard = ({ product, index }) => {
         layout: { ease: cubicBezier(0.76, 0, 0.24, 1), duration: 1.1 },
       }}
       className={`group border-gray-800 border-b relative overflow-hidden ${
-        index % 2 === 0 ? "border-r" : ""
-      } ${(index + 1) % 3 !== 0 ? "md:border-r" : "md:border-r-0"} ${
-        (index + 1) % 4 !== 0 ? "lg:border-r" : "lg:border-r-0"
-      } ${(index + 1) % 5 !== 0 ? "xl:border-r" : "xl:border-r-0"} ${
-        (index + 1) % 6 !== 0 ? "2xl:border-r" : "2xl:border-r-0"
+        index % 2 === 0 ? 'border-r' : ''
+      } ${(index + 1) % 3 !== 0 ? 'md:border-r' : 'md:border-r-0'} ${
+        (index + 1) % 4 !== 0 ? 'lg:border-r' : 'lg:border-r-0'
+      } ${(index + 1) % 5 !== 0 ? 'xl:border-r' : 'xl:border-r-0'} ${
+        (index + 1) % 6 !== 0 ? '2xl:border-r' : '2xl:border-r-0'
       }`}
       key={id}
     >
-      <Link href={`/products/${handle}`} className="custom-cursor relative">
+      <Link href={`/products/${handle}`} className='custom-cursor relative'>
         <div
-          className="group"
+          className='group'
           onMouseEnter={() => setDisplayImageUrl(imageUrl2 || imageUrl1)}
           onMouseLeave={() => setDisplayImageUrl(imageUrl1)}
         >
           {/* Mobile Product Card */}
-          <div className="md:hidden overflow-hidden text-[12px] h-[40vh] text-center flex flex-col justify-end relative">
+          <div className='md:hidden overflow-hidden text-[12px] h-[40vh] text-center flex flex-col justify-end relative'>
             <Swiper
-              className="w-full h-full object-cover object-center"
+              className='w-full h-full object-cover object-center'
               modules={[Pagination]}
-              pagination={{ clickable: true, el: ".swiper-pagination" }}
+              pagination={{ clickable: true, el: '.swiper-pagination' }}
               style={{
-                "--swiper-pagination-color": "#000",
-                "--swiper-pagination-bullet-inactive-color": "#C8C8C8",
-                "--swiper-pagination-bullet-inactive-opacity": "1",
-                "--swiper-pagination-bullet-size": "4px",
-                "--swiper-pagination-bullet-horizontal-gap": "3px",
+                '--swiper-pagination-color': '#000',
+                '--swiper-pagination-bullet-inactive-color': '#C8C8C8',
+                '--swiper-pagination-bullet-inactive-opacity': '1',
+                '--swiper-pagination-bullet-size': '4px',
+                '--swiper-pagination-bullet-horizontal-gap': '3px',
               }}
             >
               {SwiperSlides()}
-              <div className="swiper-pagination -mt- ml-6"></div>
+              <div className='swiper-pagination -mt- ml-6'></div>
             </Swiper>
 
-            <div className="flex justify-between mt-4 absolute left-3 bottom-[3px]">
+            <div className='flex justify-between mt-4 absolute left-3 bottom-[3px]'>
               <div
-                className="w-1/2 blur-2xl absolute left-0 bottom-0 h-6 z-[8]"
+                className='w-1/2 blur-2xl absolute left-0 bottom-0 h-6 z-[8]'
                 style={{
-                  backgroundColor: colorMappings[colorValue] || "#343dfb",
+                  backgroundColor: colorMappings[colorValue] || '#343dfb',
                 }}
               ></div>
-              <span className="z-[7] tracking-tighter text-left font-bold w-full leading-none">
+              <span className='z-[7] tracking-tighter text-left font-bold w-full leading-none'>
                 {title}
                 <br />
-                <span className="font-normal mt-[4px] mb-[8px] inline-block">
+                <span className='font-normal mt-[4px] mb-[8px] inline-block'>
                   {compareAtPrice && price < compareAtPrice ? (
                     <>
-                      <span className="text-gray-500 line-through pr-2">
+                      <span className='text-gray-500 line-through pr-2'>
                         {formatter.format(compareAtPrice)}
                       </span>
                       {formatter.format(price)}
@@ -132,22 +133,22 @@ const ProductCard = ({ product, index }) => {
           </div>
 
           {/* Desktop Product Card */}
-          <div className="hidden md:block w-full overflow-hidden md:h-[24rem] bg-gray-100 relative text-sm">
+          <div className='hidden md:block w-full overflow-hidden md:h-[24rem] bg-gray-100 relative text-sm'>
             <Image
               src={displayImageUrl}
-              alt={"Product Image"}
+              alt={'Product Image'}
               width={500}
               height={500}
               priority
-              className="w-full h-full object-cover object-center max-w-full"
+              className='w-full h-full object-cover object-center max-w-full'
             />
 
-            <div className="absolute left-3 font-semibold bottom-2 flex flex-col">
+            <div className='absolute left-3 font-semibold bottom-2 flex flex-col'>
               <span>{title}</span>
-              <span className="font-light -mt-1">
+              <span className='font-light -mt-1'>
                 {compareAtPrice && price < compareAtPrice ? (
                   <>
-                    <span className="text-gray-500 line-through pr-2">
+                    <span className='text-gray-500 line-through pr-2'>
                       {formatter.format(compareAtPrice)}
                     </span>
                     {formatter.format(price)}
@@ -171,7 +172,7 @@ const ProductCard = ({ product, index }) => {
         isAvailableForSale={isAvailableForSale}
       />
     </motion.div>
-  );
-};
+  )
+}
 
-export default ProductCard;
+export default ProductCard
