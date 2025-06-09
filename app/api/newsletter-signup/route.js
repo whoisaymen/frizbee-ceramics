a9df7ca08b9965b55de973618ad792da34c60716
Bhavy Jogani <bhavyjogani123@gmail.com>
2025-06-09 10:31:19 +0530

    new-api-version-test

--- app/api/newsletter-signup/route.js
+++ app/api/newsletter-signup/route.js
@@ -6,7 +6,8 @@ export async function POST(request) {
 
   const domain = process.env.SHOPIFY_STORE_DOMAIN;
   const adminAccessToken = process.env.SHOPIFY_ADMIN_ACCESSTOKEN;
-  const URL = `https://${domain}/admin/api/2023-10/customers.json`;
+  // const URL = `https://${domain}/admin/api/2023-10/customers.json`;// old api version 2023-04
+  const URL = `https://${domain}/admin/api/2024-10/customers.json`; // new api version 2023-10
 
   const options = {
     method: "POST",
@@ -18,13 +19,13 @@ export async function POST(request) {
       customer: {
         email: email,
         accepts_marketing: true,
-        // email_marketing_consent: {
-        //   state : "subscribed",
-        //   marketing_opt_in_level: "confirmed",
-        //   consent_updated_at: new Date().toISOString(),
-        //   // marketing_opt_in_level: "confirmed", //old deprecated field
-        //   // opt_in_level: "confirmed", //new field
-        // }
+        email_marketing_consent: {
+          state : "subscribed",
+          marketing_opt_in_level: "confirmed",
+          consent_updated_at: new Date().toISOString(),
+          // marketing_opt_in_level: "confirmed", //old deprecated field
+          // opt_in_level: "confirmed", //new field
+        }
       },
     }),
   };
